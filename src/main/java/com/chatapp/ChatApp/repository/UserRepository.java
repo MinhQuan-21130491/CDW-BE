5b9bc8ae8caf9efb24f8543280e47bc71e8235db
MinhQuan-21130491 <21130491@st.hcmuaf.edu.vn>
2025-03-24 22:11:20 +0700

    up jwt

--- src/main/java/com/chatapp/ChatApp/repository/UserRepository.java
+++ src/main/java/com/chatapp/ChatApp/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.chatapp.ChatApp.repository;
 import com.chatapp.ChatApp.modal.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    public Optional<User> findByEmail(String email);
 
 }
